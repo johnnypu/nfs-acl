9701fe5da05101472592fa71d6edf89eb8c21185
ubuntu <ubuntu@Johnny.>
2025-04-16 03:28:23 +0800

    replace nfsnobody to nobody user, there is no nfsnobody user

--- nfs-acl.sh
+++ nfs-acl.sh
@@ -4,8 +4,8 @@ sudo groupadd -g 1001 pod-a-group
 sudo groupadd -g 1002 pod-b-group
 
 # 設定權限與 ACL
-sudo chown -R nfsnobody:pod-a-group /var/nfsshare/pod-a
-sudo chown -R nfsnobody:pod-b-group /var/nfsshare/pod-b
+sudo chown -R nobody:pod-a-group /var/nfsshare/pod-a
+sudo chown -R nobody:pod-b-group /var/nfsshare/pod-b
 sudo chmod 770 /var/nfsshare/pod-a /var/nfsshare/pod-b
 sudo setfacl -m g:pod-a-group:rwx /var/nfsshare/pod-a
 sudo setfacl -m g:pod-b-group:rwx /var/nfsshare/pod-b
